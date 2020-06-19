2b588c67ca2129c97eedfcb2845f3a2674388d94
Ulas Tuerkmen <ulas.tuerkmen@gmail.com>
2020-06-19 13:39:21 +0200

    Added emacs list

---
+++ emacs-packages.el
@@ -0,0 +1,4 @@
+- [fiplr](https://github.com/kevinkehl/fiplr) is an interactive
+  file fuzzy-finder. If you are looking for a file in a deep
+  directory hierarchy, you can use this mode to get to it with a
+  keyword in its name.
